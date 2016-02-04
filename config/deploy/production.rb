89e3905adbc6ca51368712454e3c3647bddaafdc
Barnabas Bulpett <bbulpett@charter.net>
2016-02-04 09:41:45 -0500

    Capistrano configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@107.170.105.223}
+role :web, %w{deploy@107.170.105.223}
+role :db,  %w{deploy@107.170.105.223}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '107.170.105.223', user: 'deploy', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -26,7 +26,7 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #    auth_methods: %w(password)
 #  }
 # and/or per server
-# server 'example.com',
+# server '107.170.105.223',
 #   user: 'user_name',
 #   roles: %w{web app},
 #   ssh_options: {
